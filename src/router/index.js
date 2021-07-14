f49f739f04827e7980a15478bf70d57563131d76
passnight <2320304263@qq.com>
2021-07-14 20:07:55 +0800

    main page

--- src/router/index.js
+++ src/router/index.js
@@ -1,12 +1,17 @@
 import Vue from 'vue'
 import Router from 'vue-router'
+import Element from 'element-ui'
+import "element-ui/lib/theme-chalk/index.css"
 
 // import component
 import Content from "../components/content.vue"
 import Main from '../components/Main.vue'
 import Shit from "../components/Shit.vue"
 
+
+
 Vue.use(Router)
+Vue.use(Element)
 
 export default new Router({
   routes: [
