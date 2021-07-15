29d5202a56b7b061fb40d6bf8ed540edf5d33a7b
passnight <2320304263@qq.com>
2021-07-15 14:53:08 +0800

    init

--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -4,37 +4,26 @@ import Element from 'element-ui'
 import "element-ui/lib/theme-chalk/index.css"
 
 // import component
-import Content from "../components/content.vue"
-import Main from '../components/Main.vue'
-import Shit from "../components/Shit.vue"
 import Login from "../components/Login.vue"
+import Feedback from "../components/Feedback.vue"
 
 
 Vue.use(Router)
 Vue.use(Element)
 Vue.use(Login)
+Vue.use(Feedback)
 
 export default new Router({
   routes: [
     {
-      path: '/content',
-      name: 'content',
-      component: Content
-    },
-    {
-      path: '/Main',
-      name: 'Main',
-      component: Main
-    },
-    {
-      path: "/Shit",
-      name: "Shit",
-      component: Shit
-    },
-    {
       path: "/Login",
       name: "Login",
       component: Login
+    },
+    {
+      path: "/Feedback",
+      name: "Feedback",
+      component: Feedback
     }
   ]
 })
