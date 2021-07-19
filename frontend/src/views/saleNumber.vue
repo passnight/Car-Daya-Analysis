3901404377a8184cc2e5af4430c63120d48d0c9b
passnight <2320304263@qq.com>
2021-07-19 17:10:38 +0800

    rjx modify

--- frontend/src/views/saleNumber.vue
+++ frontend/src/views/saleNumber.vue
@@ -1,100 +1,30 @@
 <template>
-    <div id="柱状图" style="width: 100%; height: 500px;">
-    </div>
+  <div id="柱状图" style="width: 100%; height: 500px"></div>
 </template>
 
 
 <script>
-       import * as echarts from 'echarts'
-        export default {
-            data(){
-                return {
-                    chartColumn: null
-                }
-            },
-            mounted() {
-                this.drawLine();
-            },
-            methods: {
-                drawLine(){
-                    this.chartDom = echarts.init(document.getElementById('柱状图'));
+import * as echarts from "echarts";
+import axios from "axios";
+export default {
+  data() {
+    return {
+      chartColumn: null,
+      carSaleData: null,
+    };
+  },
+  mounted() {
+    axios.get("../../static/data/carSaleNumber.json").then((response) => {
+      this.carSaleData = response.data.options;
+    });
+    this.drawLine();
+  },
+  methods: {
+    drawLine() {
+      this.chartDom = echarts.init(document.getElementById("柱状图"));
 
-                    this.chartDom.setOption({
-                        tooltip: {
-        trigger: 'axis',
-        axisPointer: {
-            type: 'cross',
-            crossStyle: {
-                color: '#999'
-            }
-        }
+      this.chartDom.setOption(this.carSaleData);
     },
-    toolbox: {
-        feature: {
-            dataView: {show: true, readOnly: false},
-            magicType: {show: true, type: ['line', 'bar']},
-            restore: {show: true},
-            saveAsImage: {show: true}
-        }
-    },
-    legend: {
-        data: ['甲车', '乙车', '总体趋势'],
-         textStyle:{
-                          
-                            color: '#ffffff'//字体颜色
-                        },
-    },
-    xAxis: [
-        {
-            type: 'category',
-            data: ['1月', '2月', '3月', '4月', '5月', '6月', '7月', '8月', '9月', '10月', '11月', '12月'],
-            axisPointer: {
-                type: 'shadow'
-            }
-        }
-    ],
-    yAxis: [
-        {
-            type: 'value',
-            name: '甲车',
-            min: 0,
-            max: 250,
-            interval: 50,
-            axisLabel: {
-                formatter: '{value} 辆'
-            }
-        },
-        {
-            type: 'value',
-            name: '乙车',
-            min: 0,
-            max: 25,
-            interval: 5,
-            axisLabel: {
-                formatter: '{value} 辆'
-            }
-        }
-    ],
-    series: [
-        {
-            name: '甲车',
-            type: 'bar',
-            data: [2.0, 4.9, 7.0, 23.2, 25.6, 76.7, 135.6, 162.2, 32.6, 20.0, 6.4, 3.3]
-        },
-        {
-            name: '乙车',
-            type: 'bar',
-            data: [2.6, 5.9, 9.0, 26.4, 28.7, 70.7, 175.6, 182.2, 48.7, 18.8, 6.0, 2.3]
-        },
-        {
-            name: '总体趋势',
-            type: 'line',
-            yAxisIndex: 1,
-            data: [2.0, 2.2, 3.3, 4.5, 6.3, 10.2, 20.3, 23.4, 23.0, 16.5, 12.0, 6.2]
-        }
-    ]
-                });
-                }
-            }
-        }
-    </script>
+  },
+};
+</script>
