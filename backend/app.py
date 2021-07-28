ef67a4d762ffb251809995cf2bbc88bafd9e7d5d
passnight <2320304263@qq.com>
2021-07-28 08:46:47 +0800

    sync to  mac

--- backend/app.py
+++ backend/app.py
@@ -5,15 +5,15 @@ import json
 
 import os
 import sys
-
+current_dir = os.path.dirname(os.path.abspath(__file__))
+# 将需要导入模块代码文件相对于当前文件目录的绝对路径加入到sys.path中
+sys.path.append(os.path.join(current_dir, ".."))
 
 global feedbackSelectModel
 global feedbackSelectPriceLevel
 feedbackSelectModel = "无限制"
 feedbackSelectPriceLevel = "选项0"
-current_dir = os.path.dirname(os.path.abspath(__file__))
-# 将需要导入模块代码文件相对于当前文件目录的绝对路径加入到sys.path中
-sys.path.append(os.path.join(current_dir, ".."))
+
 from backend.Engineering import Target as target
 
 app = Flask(__name__, static_folder="http", static_url_path="/pages")
